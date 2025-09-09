47c994358b3bdb17c3e772a9c77555ac7a7d3f40
adnan <adnanadil377@gmail.com>
2025-09-09 22:53:00 +0530

    cors_allow_credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,9 +66,17 @@ REST_FRAMEWORK = {
 #     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),    # increase refresh token lifetime
 #     # other settings...
 # }
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
